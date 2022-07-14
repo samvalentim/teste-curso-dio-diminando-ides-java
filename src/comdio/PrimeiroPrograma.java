138b4b344982b317f449afd58676573b14d9b6e3
samvalentim <samanthamonteiro21@gmail.com>
2022-07-14 03:45:28 -0300

    primeiro commit

---
+++ src/comdio/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package comdio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 2;
+		int b = 3;
+		System.out.println("Hello World!" + (a+b));
+	}
+
+}
